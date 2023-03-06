25369b45aef1468f83b700e1e7e507c9e3e47ecd
Ethan Walton <ejwalton17@yahoo.com>
2023-03-05 21:36:47 -0500

    adding get cmd

--- cmd/get.go
+++ cmd/get.go
@@ -1,11 +1,13 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -13,15 +15,45 @@ import (
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "This command will get the desired gopher wantes",
+	Long:  `This command under the hood will call another repo to return the gopher.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("get called")
+		var gopherName = "gophygoph"
+
+		if len(args) >= 1 && args[0] != "" {
+			gopherName = args[0]
+		}
+
+		URL := "https://github.com/scraly/gophers/raw/main/" + gopherName + ".png"
+
+		fmt.Println("Trying to get '" + gopherName + "' Gopher....")
+
+		//getting the gopher data now
+		response, err := http.Get(URL)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer response.Body.Close()
+
+		if response.StatusCode == 200 {
+			out, err := os.Create(gopherName + ".png")
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer out.Close()
+
+			//write the body to file
+			_, err = io.Copy(out, response.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println("Perfect! Just saved in" + out.Name() + "!")
+		} else {
+			fmt.Println("Error: " + gopherName + " does not exists! :-(")
+		}
 	},
 }
 
